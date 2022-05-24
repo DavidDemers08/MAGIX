1eb41b5e6a3f054d649124ee65893a1243008bb7
1942878 <e.ddemers3@etu.cvm.qc.ca>
2022-05-24 11:47:20 -0400

    COMMIT FINAL

--- examen1/examen/question2/index.php
+++ examen1/examen/question2/index.php
@@ -4,6 +4,9 @@
 	$action = new IndexAction();
 	$data = $action->execute();
 
+	
+	$donnees =  json_encode($data["result"]);
+	
 
 	$pageTitle = "Accueil";
 	require_once("partial/header.php");
@@ -11,15 +14,27 @@
 	<div class="container">
 		<table>
 			<tr>
-				<th>Date</th>
+					<th>Date</th>
 				<th>Gagnant</th>
 				<th>Perdant</th>
+					<?php
+						for ($i=0; $i < 20; $i++) {
+							?>
+								<tr>
+									<th><?=$data["result"][$i]->date?></th>
+									<th><?=$data["result"][$i]->winner?></th>
+									<th><?=$data["result"][$i]->looser?></th>
+								</tr>
+							<?php
+							
+						}
+					?>
+							
+
+
+				
 			</tr>
-			<tr>
-				<td>Exemple 1</td>
-				<td>Exemple 2</td>
-				<td>Exemple 3</td>
-			</tr>
+			
 		</table>
 		<div class="btn-container">
 			<button>
